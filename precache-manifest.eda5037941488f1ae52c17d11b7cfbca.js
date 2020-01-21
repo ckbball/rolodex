1ca49cb3edcf9b52c117f3aae93f6ce3ffb819cf
Caesar Ladion <caesar.ladion@gmail.com>
2020-01-20 22:34:16 -0800

    Updates

---
+++ precache-manifest.eda5037941488f1ae52c17d11b7cfbca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c236d0da5fe83a68711722cfbd41514",
+    "url": "/rolodex/index.html"
+  },
+  {
+    "revision": "1a81e0053c89efcd72f6",
+    "url": "/rolodex/static/css/main.7005782b.chunk.css"
+  },
+  {
+    "revision": "8557bd2753dae7ad6ea1",
+    "url": "/rolodex/static/js/2.5d5e4d03.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/rolodex/static/js/2.5d5e4d03.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1a81e0053c89efcd72f6",
+    "url": "/rolodex/static/js/main.539f1ceb.chunk.js"
+  },
+  {
+    "revision": "695a2c3aaef1ea6abc9a",
+    "url": "/rolodex/static/js/runtime-main.2a3f69e6.js"
+  }
+]);
